c09787774ce36e129bba6c693b3663b9d46d5c6d
jaean123 <whileloops@gmail.com>
2018-05-22 15:51:07 -0400

    Initial commit

---
+++ DeadReckoner.h
@@ -0,0 +1,37 @@
+/*
+ Name:		DeadReckoner.h
+ Created:	5/18/2018 4:58:56 PM
+ Author:	while
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _DeadReckoner_h
+#define _DeadReckoner_h
+#include <Arduino.h>
+
+class DeadReckoner {
+
+public:
+	DeadReckoner(int& left, int& right, double tpr, double r, double l);
+	void integratePosition();
+	void computeAngularVelocities();
+	double getX();
+	double getY();
+	double getWl();
+	double getWr();
+
+private:
+	int& leftTicks, rightTicks; // Number of total wheel encoder tick counts for left and right wheels.
+	int leftTicksPrev, rightTicksPrev; // Number of total wheel encoder tick counts at time computeAngularVelocities() is called.
+	double xc, yc; // Latest position coordinates in ticks.
+	double wl, wr; // Latest left and right angular velocity of the wheels in radians per second.
+	double ticksPerRev; // Number of tick registers per second of the encoder.
+	double length; // Length from left wheel to right wheel.
+	double radius; // Radius of the wheel.
+	double theta;
+	unsigned long prevIntegrationTime;
+	unsigned long prevWheelComputeTime;
+
+};
+
+#endif
