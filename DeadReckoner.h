76f58bf8dff7b75a4ce4c34ec5f429e7b0373e64
jaean123 <whileloops@gmail.com>
2018-05-23 14:41:48 -0400

    Added getTheta method

--- DeadReckoner.h
+++ DeadReckoner.h
@@ -1,26 +1,22 @@
-/*
-Name:		DeadReckoner.h
-Created:	5/18/2018 4:58:56 PM
-Author:	while
-Editor:	http://www.visualmicro.com
-*/
 
 #ifndef _DeadReckoner_h
 #define _DeadReckoner_h
+
 #include <Arduino.h>
 
 class DeadReckoner {
 
 public:
 	DeadReckoner(volatile int *, volatile int *, double, double, double);
-	void integratePosition();
-	void computeAngularVelocities();
+	void computePosition();
 	double getX();
 	double getY();
 	double getWl();
 	double getWr();
+	double getTheta();
 
 private:
+	void computeAngularVelocities();
 	volatile int *leftTicks, *rightTicks; // Number of total wheel encoder tick counts for left and right wheels.
 	int leftTicksPrev, rightTicksPrev; // Number of total wheel encoder tick counts at time computeAngularVelocities() is called.
 	double xc, yc; // Latest position coordinates in ticks.
