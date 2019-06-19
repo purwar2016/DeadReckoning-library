f93ddf3e5c8671127b0548797ab17549c5b9795e
jaean123 <whileloops@gmail.com>
2019-06-19 15:43:01 -0400

    computePosition method changed and bug fixes
    comptuePosition method will run at the set interval. No need to manually call the method at a specified interval. Just call it in the loop frequently.

--- DeadReckoner.cpp
+++ DeadReckoner.cpp
@@ -2,10 +2,25 @@
 #include "DeadReckoner.h"
 #include <Arduino.h>
 
-DeadReckoner::DeadReckoner(volatile unsigned long *left, volatile unsigned long *right, double tpr, double r, double l) {
+DeadReckoner::DeadReckoner(volatile unsigned long *left, volatile unsigned long *right, unsigned long ci, int tpr, double r, double l) {
 	leftTicks = left;
 	rightTicks = right;
 	ticksPerRev = tpr;
+	positionComputeInterval = ci*1000; // millisecond to microseconds
+	radius = r;
+	length = l;
+	toRadPerSec = 1000000.0 * TWO_PI / ticksPerRev;
+}
+
+DeadReckoner::DeadReckoner() {
+	
+}
+
+void DeadReckoner::setParams(volatile unsigned long *left, volatile unsigned long *right, unsigned long ci, double tpr, double r, double l) {
+	leftTicks = left;
+	rightTicks = right;
+	ticksPerRev = tpr;
+	positionComputeInterval = ci;
 	radius = r;
 	length = l;
 	toRadPerSec = 1000000 * TWO_PI / ticksPerRev;
@@ -33,16 +48,16 @@ double DeadReckoner::getY() {
 
 double DeadReckoner::getWl() {
 	// TODO: Check this
-	return wl * dt_omega / (UNSIGNED_LONG_MAX - dt_omega);
+	return wl;
 }
 
 double DeadReckoner::getWr() {
 	// TODO: Check this
-	return wr * dt_omega / (UNSIGNED_LONG_MAX - dt_omega);
+	return wr;
 }
 
 double DeadReckoner::getW() {
-	return w * dt_integration / (UNSIGNED_LONG_MAX - dt_integration);
+	return w;
 }
 
 double DeadReckoner::getTheta() {
@@ -68,7 +83,7 @@ int DeadReckoner::getRightOmegaDirection() {
 void DeadReckoner::computeAngularVelocities() {
 	// Time elapsed after computing the angular velocity previously.
 	// change in time is defined as previous - current to prevent round off error.
-	dt_omega = getChange(prevWheelComputeTime, micros()); // in milliseconds
+	dt_omega = getChange(micros(), prevWheelComputeTime); // in milliseconds
 
 	double changeLeftTicks = getChange(*leftTicks, leftTicksPrev);
 	double changeRightTicks = getChange(*rightTicks, rightTicksPrev);
@@ -89,49 +104,42 @@ void DeadReckoner::computeAngularVelocities() {
 	prevWheelComputeTime = micros();
 }
 
-unsigned long DeadReckoner::getChange(unsigned long current, unsigned long previous) {
-	// Overflow has occured
-	if (current < previous) {
-		return UNSIGNED_LONG_MAX - previous + current;
-	}
-	// No overflow
-	return current - previous;
-}
-
 void DeadReckoner::computePosition() {
-	computeAngularVelocities();
-	// Time elapsed after the previous position has been integrated.
-	// change in time is defined as previous - current to prevent round off error.
-	dt_integration = getChange(prevIntegrationTime, micros());
+	if (micros() - prevIntegrationTime > positionComputeInterval) {
+		computeAngularVelocities();
+		// Time elapsed after the previous position has been integrated.
+		// change in time is defined as previous - current to prevent round off error.
+		dt_integration = getChange(micros(), prevIntegrationTime);
 
-	float dt = dt_integration / 1000000.0; // convert to seconds
+		float dt = dt_integration / 1000000.0; // convert to seconds
 
-	// Dead reckoning equations
+		// Dead reckoning equations
 
-	float Vl = wl * radius;
-	float Vr = wr * radius;
-	float v = (Vr + Vl) / 2.0;
-	w = (Vr - Vl) / length;
-	// Uses 4th order Runge-Kutta to integrate numerically to find position.
-	float xNext = xc + dt * v*(2 + cos(dt*w / 2))*cos(theta + dt * w / 2) / 3;
-	float yNext = yc + dt * v*(2 + cos(dt*w / 2))*sin(theta + dt * w / 2) / 3;
-	float thetaNext = theta + dt * w;
+		float Vl = wl * radius;
+		float Vr = wr * radius;
+		float v = (Vr + Vl) / 2.0;
+		w = (Vr - Vl) / length;
+		// Uses 4th order Runge-Kutta to integrate numerically to find position.
+		float xNext = xc + dt * v*(2 + cos(dt*w / 2))*cos(theta + dt * w / 2) / 3;
+		float yNext = yc + dt * v*(2 + cos(dt*w / 2))*sin(theta + dt * w / 2) / 3;
+		float thetaNext = theta + dt * w;
 
-	xc = xNext;
-	yc = yNext;
-	theta = thetaNext;
+		xc = xNext;
+		yc = yNext;
+		theta = thetaNext;
 
-	float toRPM = 30 / PI;
-	float dist = sqrt(xc*xc + yc * yc);
-	//Serial.print("\tdist: "); Serial.print(dist);
-	//Serial.print("\twl: "); Serial.print(wl*toRPM, 5);
-	//Serial.print("\twr: "); Serial.print(wr*toRPM, 5);
-	//Serial.print("\tVl: "); Serial.print(Vl);
-	//Serial.print("\tVr: "); Serial.print(Vr);
-	//Serial.print("\tw: "); Serial.print(w, 5);
-	//Serial.print("\tx: "); Serial.print(xc);
-	//Serial.print("\ty: "); Serial.print(yc);
-	//Serial.print("\ttheta: "); Serial.println(theta*RAD_TO_DEG);
+		float toRPM = 30 / PI;
+		float dist = sqrt(xc*xc + yc * yc);
+		//Serial.print("\tdist: "); Serial.print(dist);
+		//Serial.print("\twl: "); Serial.print(wl*toRPM, 5);
+		//Serial.print("\twr: "); Serial.print(wr*toRPM, 5);
+		//Serial.print("\tVl: "); Serial.print(Vl);
+		//Serial.print("\tVr: "); Serial.print(Vr);
+		//Serial.print("\tw: "); Serial.print(w, 5);
+		//Serial.print("\tx: "); Serial.print(xc);
+		//Serial.print("\ty: "); Serial.print(yc);
+		//Serial.print("\ttheta: "); Serial.println(theta*RAD_TO_DEG);
 
-	prevIntegrationTime = micros();
+		prevIntegrationTime = micros();
+	}
 }
