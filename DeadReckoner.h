96bf5237cfd22134ed563d83147552acd5b7fc88
jaean123 <whileloops@gmail.com>
2019-06-19 15:43:38 -0400

    computePosition method changed and bug fixes
    comptuePosition method will run at the set interval. No need to manually call the method at a specified interval. Just call it in the loop frequently.

--- DeadReckoner.h
+++ DeadReckoner.h
@@ -11,7 +11,9 @@
 class DeadReckoner {
 
 public:
-	DeadReckoner(volatile unsigned long *, volatile unsigned long *, double, double, double);
+	DeadReckoner(volatile unsigned long *, volatile unsigned long *, unsigned long, int, double, double);
+	DeadReckoner();
+	void setParams(volatile unsigned long *, volatile unsigned long *, unsigned long, double, double, double);
 	void computePosition();
 	void setX(double);
 	void setY(double);
@@ -29,12 +31,11 @@ public:
 
 private:
 	void computeAngularVelocities();
-	unsigned long getChange(unsigned long, unsigned long);
 	volatile unsigned long *leftTicks, *rightTicks; // Number of total wheel encoder tick counts for left and right wheels.
 	unsigned long leftTicksPrev, rightTicksPrev; // Number of total wheel encoder tick counts at time computeAngularVelocities() is called.
 	double xc, yc; // Latest position coordinates in ticks.
 	double wl, wr; // Latest left and right angular velocity of the wheels in radians per second.
-	double ticksPerRev; // Number of tick registers per second of the encoder.
+	int ticksPerRev; // Number of tick registers per second of the encoder.
 	float w; // Angular velocity of the robot.
 	double length; // Length from left wheel to right wheel.
 	double radius; // Radius of the wheel.
@@ -42,10 +43,20 @@ private:
 	double toRadPerSec; // ticks/microsecond to rad/s conversion factor
 	unsigned long prevIntegrationTime;
 	unsigned long prevWheelComputeTime;
+	unsigned long positionComputeInterval;
 	int leftOmegaDirection = 1;
 	int rightOmegaDirection = 1;
 	unsigned long dt_omega;
 	unsigned long dt_integration;
+
+	unsigned long static DeadReckoner::getChange(unsigned long current, unsigned long previous) {
+		// Overflow has occured
+		if (current < previous) {
+			return UNSIGNED_LONG_MAX - previous + current;
+		}
+		// No overflow
+		return current - previous;
+	}
 };
 
 #endif
